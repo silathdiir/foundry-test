d55709e8a1afbe4cf89bef725eb615ea71ebef3d
Steven Gu <asongala@163.com>
2024-06-21 16:19:48 +0800

    Add a simple contract and deploy.

---
+++ test-contracts/script/DeploySimple.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/Simple.sol";
+
+contract DeploySimple is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        Simple simple = new Simple();
+
+        vm.stopBroadcast();
+    }
+}
