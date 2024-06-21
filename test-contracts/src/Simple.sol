02a6c8fd2bb32a1c6dae3411a31293eabb48d754
Steven Gu <asongala@163.com>
2024-06-21 19:06:10 +0800

    Update the simple slot.

--- test-contracts/src/Simple.sol
+++ test-contracts/src/Simple.sol
@@ -2,13 +2,30 @@
 pragma solidity ^0.8.13;
 
 contract Simple {
-    uint256 public number;
+    // Test simple slots (slot 0 - 3)
+    bool public s1;
+    uint256 public s2;
+    string public s3;
+    address public s4;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    // Test mapping slots (slot 4)
+    mapping(address => uint256) public m1;
+
+    // Set the simple slots.
+    function setSimples(
+        bool newS1,
+        uint256 newS2,
+        string memory newS3,
+        address newS4
+    ) public {
+        s1 = newS1;
+        s2 = newS2;
+        s3 = newS3;
+        s4 = newS4;
     }
 
-    function increment() public {
-        number++;
+    // Set a mapping slot by key and value.
+    function setMapping(address key, uint256 value) public {
+        m1[key] = value;
     }
 }
