4bf7dc7df915f3523f807cef3e5c3033f9ae2cfd
Steven Gu <asongala@163.com>
2024-06-21 17:24:20 +0800

    Add cast script.

--- test-contracts/script/DeploySimple.s.sol
+++ test-contracts/script/DeploySimple.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/Simple.sol";
+import "../../src/Simple.sol";
 
 contract DeploySimple is Script {
     function run() external {
