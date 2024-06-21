27ac8a01930a2e1ac061991211519a96e9692e02
Steven Gu <asongala@163.com>
2024-06-21 22:26:25 +0800

    Add import script.

--- test-contracts/cast/init_simple.sh
+++ test-contracts/cast/init_simple.sh
@@ -1,3 +1,35 @@
 
-cast send 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 "setNumber(uint256)" 0x000000000000000000000000000000000000000000000000000000000001e240 --rpc-url http://localhost:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+cast send \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "setSimples(bool,uint256,string,address)" \
+    true "100" "test" 0xb90ed61bffed1df72f2ceebd965198ad57adfcbd \
+    --rpc-url http://localhost:8545 \
+    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
+cast send \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "setMapping(address,uint256)" \
+    0x3bf5733f695b2527acc7bd4c5350e57acfd9fbb5 100 \
+    --rpc-url http://localhost:8545 \
+    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+cast send \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "setMapping(address,uint256)" \
+    0x6cac7190535f4908d0524e7d55b3750376ea1ef7 200 \
+    --rpc-url http://localhost:8545 \
+    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+cast send \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "setMapping(address,uint256)" \
+    0x90f2e2a59e125b11635dc80f6a28002dea80c138 300 \
+    --rpc-url http://localhost:8545 \
+    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+cast send \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "setMapping(address,uint256)" \
+    0x017e305b9c15a72acca623c26e0e574aa51eb023 400 \
+    --rpc-url http://localhost:8545 \
+    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
--- test-contracts/script/DeploySimple.s.sol
+++ test-contracts/script/DeploySimple.s.sol
@@ -2,13 +2,27 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../../src/Simple.sol";
+import "../src/Simple.sol";
 
 contract DeploySimple is Script {
     function run() external {
         vm.startBroadcast();
 
-        Simple simple = new Simple();
+        string memory saltMsg = "mp2-v1";
+        bytes32 salt = keccak256(abi.encodePacked(saltMsg));
+
+        // Get the bytecode of the contract.
+        bytes memory bytecode = type(Simple).creationCode;
+        bytecode = abi.encodePacked(bytecode, abi.encode(saltMsg));
+
+        // Deploy the contract using CREATE2.
+        address addr;
+        assembly {
+            addr := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
 
         vm.stopBroadcast();
     }
