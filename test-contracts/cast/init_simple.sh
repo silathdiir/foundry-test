ffa21777904eb11d0fd1f392349cf0b000c2f144
Steven Gu <asongala@163.com>
2024-06-22 10:57:53 +0800

    Check the import data.

--- test-contracts/cast/init_simple.sh
+++ test-contracts/cast/init_simple.sh
@@ -1,4 +1,3 @@
-
 cast send \
     0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
     "setSimples(bool,uint256,string,address)" \
@@ -33,3 +32,29 @@ cast send \
     0x017e305b9c15a72acca623c26e0e574aa51eb023 400 \
     --rpc-url http://localhost:8545 \
     --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+cast call \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "s1()" \
+    --rpc-url http://localhost:8545
+
+cast call \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "s2()" \
+    --rpc-url http://localhost:8545
+
+cast call \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "s3()" \
+    --rpc-url http://localhost:8545
+
+cast call \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "s4()" \
+    --rpc-url http://localhost:8545
+
+cast call \
+    0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c \
+    "m1(address)(uint256)" \
+    0x3bf5733f695b2527acc7bd4c5350e57acfd9fbb5 \
+    --rpc-url http://localhost:8545
