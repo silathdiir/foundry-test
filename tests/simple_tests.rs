bd4294f4b165fb8dec962a290c7169c8513bd2f8
Steven Gu <asongala@163.com>
2024-06-22 21:59:46 +0800

    Add CI.

--- tests/simple_tests.rs
+++ tests/simple_tests.rs
@@ -9,7 +9,7 @@ const RPC_URL: &str = "http://localhost:8545";
 #[tokio::test]
 async fn simple_test() {
     let rpc = Provider::<Http>::try_from(RPC_URL).unwrap();
-    let contract = Address::from_str("0x7A6A4E38038BCdFA55Ba3Fb4BAd4D735233b3E7c").unwrap();
+    let contract = Address::from_str("0x9ec1BCbcC362942Ed7C1C999977623239aCe030F").unwrap();
     let location = H256::from_low_u64_be(0);
     let res = rpc.get_proof(contract, vec![location], None).await.unwrap();
     println!("res = {res:?}");
